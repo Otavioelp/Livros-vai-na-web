5918fda3ac50c69592f9a1dbbd63ee58c9820022
Otavioelp <otavioelpidio123@gmail.com>
2024-12-03 14:37:28 -0300

    primeiro commit

---
+++ ProjetoFinal/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './componentes/header/Header'
+import Footer from './componentes/footer/footer'
+import './GlobalStyle/globalStyle.scss';
+
+
+export default function App() {
+
+  return(
+<section>
+    <Header />
+    <Footer />
+    </section>
+  )
+}
